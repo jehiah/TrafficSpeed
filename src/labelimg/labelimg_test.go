abf1a3bacb138af228c3f6595bacc8c67b4da43f
Jehiah Czebotar <jehiah@gmail.com>
2017-05-29 13:32:21 -0400

    start lableimg tests

---
+++ src/labelimg/labelimg_test.go
@@ -0,0 +1,25 @@
+package labelimg
+
+import (
+	"testing"
+	"image"
+	
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLabelimg(t *testing.T) {
+	
+	l := &image.Gray{
+		Pix: []uint8{3, 3, 0, 4,
+     			3, 0, 4, 4,
+		},
+		Stride: 4,
+		Rect: image.Rect(1, 1, 5, 3), //  4 x 2
+	}	
+	
+	labeled := New(l)
+	
+	assert.Equal(t, []uint8{1, 1, 0, 2,
+						1, 0, 2, 2}, labeled.Pix)
+	
+}
--- vendor.sh
+++ vendor.sh
@@ -5,3 +5,5 @@ gb vendor fetch gopkg.in/gographics/imagick.v3
 gb vendor fetch -revision 3bf12259b1cd565687ea00c9e5a6b648868133c5 github.com/chai2010/webp
 gb vendor fetch golang.org/x/image/bmp
 gb vendor fetch -revision 7f2c395b16042d36ab187448a70b9de9ccb5854d github.com/anthonynsimon/bild
+
+gb vendor fetch github.com/stretchr/testify/assert
